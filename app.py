d5c193236ac5f7867f1ab23014abe784c16f993e
issou2025 <bacseried@gmail.com>
2025-06-14 17:07:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -747,6 +747,7 @@ def admin_site():
     </div>
     """
     return render(content, page="admin")
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
